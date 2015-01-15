d3957638af05ef12c3bbb269f956d27655bae28e
Andrea Formica <andrea.formica@cern.ch>
2015-01-15 11:22:01 +0100

    testing configuration for jetty and other things

--- CondDBWeb/src/main/java/conddb/dao/repositories/GlobalTagRepository.java
+++ CondDBWeb/src/main/java/conddb/dao/repositories/GlobalTagRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 import conddb.data.GlobalTag;
 
@@ -17,6 +18,7 @@ import conddb.data.GlobalTag;
  *
  */
 @RepositoryRestResource
+@Transactional(readOnly=true)
 public interface GlobalTagRepository extends CrudRepository<GlobalTag, String> {
 
 	List<GlobalTag> findByNameLike(@Param("name")String name);
