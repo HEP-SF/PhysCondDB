ffca6ca3519fe75ef0716f110e0a2e0ab67526f8
AFormic <aformic@Andreas-MacBook-Pro.local>
2015-08-27 22:18:31 +0200

    Add new setup file for linking properties into sub directories

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+alldirs=`ls -1`
+alldirs=`find . -name "pom.xml" -print | egrep -v "target" | awk -F'/' '{print $2}' | egrep -v "pom"`
+wdir=$PWD
+
+# Change this variables to point to your local installation
+
+for adir in $alldirs; do
+echo "present directory is $wdir: examine $adir"
+echo "linking properties..." 
+cd $adir
+if [ ! -e conddb-filter-values.properties ]; then
+ echo "linking properties file"
+ ln -s $wdir/conddb-filter-values.properties .
+fi
+cd $wdir
+done
+
