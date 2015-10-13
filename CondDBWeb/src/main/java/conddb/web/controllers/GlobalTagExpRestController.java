bc86111b0910f5632fcd830af15840aa85ced830
Andrea Formica <andrea.formica@cern.ch>
2015-10-13 20:00:37 +0200

    Add expert resources.

--- CondDBWeb/src/main/java/conddb/web/controllers/GlobalTagExpRestController.java
+++ CondDBWeb/src/main/java/conddb/web/controllers/GlobalTagExpRestController.java
@@ -6,7 +6,6 @@ package conddb.web.controllers;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -17,6 +16,7 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -31,7 +31,6 @@ import org.springframework.stereotype.Controller;
 import conddb.dao.admin.controllers.GlobalTagAdminController;
 import conddb.dao.controllers.GlobalTagService;
 import conddb.dao.exceptions.ConddbServiceException;
-import conddb.data.ErrorMessage;
 import conddb.data.GlobalTag;
 import conddb.data.GlobalTagMap;
 import conddb.data.GlobalTagStatus;
@@ -43,6 +42,9 @@ import conddb.web.exceptions.ConddbWebException;
 import conddb.web.resources.GlobalTagResource;
 import conddb.web.resources.Link;
 import conddb.web.resources.SpringResourceFactory;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * @author aformic
@@ -50,6 +52,7 @@ import conddb.web.resources.SpringResourceFactory;
  */
 @Path(Link.EXPERT)
 @Controller
+@Api(value = Link.EXPERT)
 public class GlobalTagExpRestController extends BaseController {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
@@ -64,62 +67,55 @@ public class GlobalTagExpRestController extends BaseController {
 	private TimestampDeserializer timestampDeserializer;
 
 	@POST
-	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path(Link.GLOBALTAGS)
-	public Response create(@Context UriInfo info, GlobalTag globaltag) throws ConddbWebException {
+	@ApiOperation(value = "Create a GlobalTag.",
+    notes = "Input data are in json, and should match all needed fields for a new global tag.",
+    response=GlobalTagResource.class)
+	public Response createGlobalTag(@Context UriInfo info,
+			GlobalTag globaltag) throws ConddbWebException {
 		try {
+//			GlobalTag globaltag = new GlobalTag();
 			GlobalTag saved = globalTagService.insertGlobalTag(globaltag);
 			saved.setResId(saved.getName());
 			GlobalTagResource resource = (GlobalTagResource) springResourceFactory.getResource("globaltag", info,
 					saved);
 			return created(resource);
 		} catch (ConddbServiceException e) {
-			ConddbWebException ex = new ConddbWebException();
-			ErrorMessage error = new ErrorMessage("Error creating new GlobalTag resource "+globaltag.getName());
-			error.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
-			error.setInternalMessage("Cannot create new global tag :"+e.getMessage());
-			ex.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
-			ex.setErrMessage(error);
-			throw ex;
+			String msg = "Error crerating globaltag resource using "+globaltag.toString();
+			throw buildException(msg+" "+e.getMessage(), msg, Response.Status.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@Path(Link.GLOBALTAGS+"/{id}")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateGlobalTag(@Context UriInfo info, @PathParam("id") String id, Map map)
+	@ApiOperation(value = "Update a GlobalTag.",
+		    notes = "Input data are in json, and should match all  fields to be updated.")
+	public Response updateGlobalTag(@Context UriInfo info, 
+			@ApiParam(value = "id: id of the globaltag to be updated", required = true) 
+			@PathParam("id") String id, 
+			Map map)
 			throws ConddbWebException {
 		Response resp;
 		try {
-			ConddbWebException ex = new ConddbWebException();
 			log.info("Request for updating global tag "+id+" using "+map.size());
 			GlobalTag existing = globalTagService.getGlobalTag(id);
 			if (existing == null) {
-				ErrorMessage error = new ErrorMessage("Error updating not existing GlobalTag resource "+id);
-				error.setCode(Response.Status.NOT_FOUND.getStatusCode());
-				error.setInternalMessage("Cannot update global tag because is not found in DB");
-				ex.setStatus(Response.Status.NOT_FOUND);
-				ex.setErrMessage(error);
-				throw ex;
+				String msg = "Error updating globaltag resource: id "+id+" not found!";
+				throw buildException(msg, msg, Response.Status.NOT_FOUND);
 			}
 			//TODO: This should be activated in the final code
 			if (existing.islocked()) {
-				ErrorMessage error = new ErrorMessage("Error updating locked GlobalTag resource "+id);
-				error.setCode(Response.Status.NOT_MODIFIED.getStatusCode());
-				error.setInternalMessage("Cannot update global tag because it is locked");
-				ex.setStatus(Response.Status.NOT_MODIFIED);
-				ex.setErrMessage(error);
-				log.info("In principle no update is possible for a locked global tag");
+				String msg = "Error updating locked globaltag resource: id "+id+" is locked!";
+				throw buildException(msg, msg, Response.Status.NOT_MODIFIED);
 			}
 			if (map.containsKey("name")) {
 				List<GlobalTagMap> maplist = globalTagService.getGlobalTagMapByGlobalTagName(id);
 				if (maplist != null && maplist.size()>0 || existing.islocked()) {
-					ErrorMessage error = new ErrorMessage("Cannot update GlobalTag resource <name> field when associations are active..."+id);
-					error.setCode(Response.Status.NOT_MODIFIED.getStatusCode());
-					error.setInternalMessage("Cannot update global tag because <name> field cannot be modified");
-					ex.setStatus(Response.Status.NOT_MODIFIED);
-					ex.setErrMessage(error);
-					throw ex;
+					String msg = "Error updating locked globaltag resource: <name> cannot be modified!";
+					throw buildException(msg, msg, Response.Status.NOT_MODIFIED);
 				}
 				existing.setName(String.valueOf(map.get("name")));
 			}
@@ -155,28 +151,29 @@ public class GlobalTagExpRestController extends BaseController {
 			return resp;
 		} catch (ConddbServiceException e) {
 			log.debug("Generate exception using an ConddbService exception..."+e.getMessage());
-			ConddbWebException ex = new ConddbWebException(e.getMessage());
-			ex.setStatus(Response.Status.BAD_REQUEST);
-			throw ex;
+			String msg = "Error updating locked globaltag resource: internal server exception !";
+			throw buildException(msg+" \n "+e.getMessage(), msg, Response.Status.INTERNAL_SERVER_ERROR);
 		} catch (ConversionException e) {
 			log.debug("Generate exception using an ConversionException exception..."+e.getMessage());
-			ConddbWebException ex = new ConddbWebException(e.getMessage());
-			ex.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
-			throw ex;
+			String msg = "ConversionException updating locked globaltag resource:";
+			throw buildException(msg+" \n "+e.getMessage(), msg, Response.Status.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@Path(Link.GLOBALTAGS+"/maps/{id}")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Map a tag to a globaltag.",
+    notes = "Can be used for one or many tags.")
 	public Response mapTagsToGlobalTag(
 			@Context UriInfo info, 
+			@ApiParam(value = "id: id of the globaltag to be updated", required = true) 
 			@PathParam("id") String id, 
+			@ApiParam(value = "action: {addtags|merge} default is addtags ", required = true) 
 			@DefaultValue("addtags") @QueryParam("action") final String action,
 			Map map)
 			throws ConddbWebException {
 		Response resp;
-		ConddbWebException ex = new ConddbWebException();
 		try {
 			String record = (map.containsKey("record")) ? (String) map.get("record") : "none" ;
 			String label = (map.containsKey("label")) ? (String) map.get("label") : "none" ;
@@ -184,31 +181,19 @@ public class GlobalTagExpRestController extends BaseController {
 			if (map.containsKey("name")) {
 				pattern = (String)map.get("name");
 			} else {
-				ErrorMessage error = new ErrorMessage("Cannot create mappings when <name> field is null..."+id);
-				error.setCode(Response.Status.NOT_MODIFIED.getStatusCode());
-				error.setInternalMessage("Cannot create mapping because <name> field for tag pattern is null");
-				ex.setStatus(Response.Status.NOT_MODIFIED);
-				ex.setErrMessage(error);
-				throw ex;			
+				String msg = "Error creating mapping: <name> cannot be null!";
+				throw buildException(msg, msg, Response.Status.NOT_MODIFIED);		
 			}
 			log.info("Request to associate tags to a global tag "+id+" using a pattern "+pattern);
 			log.info("   - using common values for record and label :"+record+" "+label);
 			GlobalTag existing = globalTagService.getGlobalTag(id);
 			if (existing == null) {
-				ErrorMessage error = new ErrorMessage("Cannot create mappings because global tag resource was not found for id="+id);
-				error.setCode(Response.Status.NOT_FOUND.getStatusCode());
-				error.setInternalMessage("Cannot create mapping because global tag resource was not found in the DB");
-				ex.setStatus(Response.Status.NOT_FOUND);
-				ex.setErrMessage(error);
-				throw ex;			
+				String msg = "Error creating mapping, globaltag resource with id "+id+" not found!";
+				throw buildException(msg, msg, Response.Status.NOT_FOUND);
 			}
 			if (existing.islocked()) {
-				ErrorMessage error = new ErrorMessage("Cannot create mappings because global tag "+id+" is locked");
-				error.setCode(Response.Status.NOT_ACCEPTABLE.getStatusCode());
-				error.setInternalMessage("Cannot create mapping because global tag resource is locked in the DB");
-				ex.setStatus(Response.Status.NOT_ACCEPTABLE);
-				ex.setErrMessage(error);
-				throw ex;			
+				String msg = "Error in add a mapping to a locked globaltag resource: "+existing.toString();
+				throw buildException(msg, msg, Response.Status.NOT_MODIFIED);
 			}
 			if (action.equals("addtags")) {
 				log.info("action=addtags: add to "+id+" using tag pattern "+pattern);
@@ -219,7 +204,6 @@ public class GlobalTagExpRestController extends BaseController {
 				}
 			} else if (action.equals("merge")) {
 				log.info("action=merge: add to "+id+" using global tag name "+pattern);
-
 				// In this case pattern indicates another global tag name
 				GlobalTag sourcegtag = globalTagService.getGlobalTagFetchTags(pattern);
 				Set<GlobalTagMap> tagmaplist = sourcegtag.getGlobalTagMaps();
@@ -228,45 +212,38 @@ public class GlobalTagExpRestController extends BaseController {
 					globalTagService.mapAddTagToGlobalTag(tag, existing,record, label);
 				}
 			}
-			resp = Response.ok().build();
+			resp = Response.ok(existing).build();
 			return resp;
 
 		} catch (ConddbServiceException e) {
-			ErrorMessage error = new ErrorMessage("Cannot create mappings because internal service received exception for global tag "+id);
-			error.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
-			error.setInternalMessage("Cannot create mapping because of an internal server error : "+e.getMessage());
-			ex.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
-			ex.setErrMessage(error);
-			throw ex;			
+			log.debug("Generate exception using an ConddbService exception..."+e.getMessage());
+			String msg = "Error creating mappings for globaltag resource: internal server exception !";
+			throw buildException(msg+" \n "+e.getMessage(), msg, Response.Status.INTERNAL_SERVER_ERROR);	
 		}
 	}
 
 	@Path(Link.GLOBALTAGS+"/{id}")
 	@DELETE
-	public Response deleteGlobalTag(@PathParam("id") String id) throws ConddbWebException {
+	@ApiOperation(value = "Delete a globaltag.",
+    notes = "It should be used one global tag at the time. This method is meant for administration purposes.")
+	public Response deleteGlobalTag(
+			@ApiParam(value = "id: id of the globaltag to be deleted", required = true) 
+			@PathParam("id") String id) throws ConddbWebException {
 		Response resp;
-		ConddbWebException ex = new ConddbWebException();
 		try {
 			GlobalTag existing = globalTagService.getGlobalTag(id);
 			if (existing.islocked()) {
-				ErrorMessage error = new ErrorMessage("Cannot remove global tag "+id+" because it is locked");
-				error.setCode(Response.Status.NOT_MODIFIED.getStatusCode());
-				error.setInternalMessage("Cannot remove a locked global tag");
-				ex.setStatus(Response.Status.NOT_MODIFIED);
-				ex.setErrMessage(error);
+				String msg = "Error in removing a locked globaltag resource: "+existing.toString();
+				throw buildException(msg, msg, Response.Status.NOT_MODIFIED);
 			}
 			existing = globalTagAdminController.deleteGlobalTag(id);
 			resp = Response.ok(existing).build();
-
+			return resp;
 		} catch (ConddbServiceException e) {
-			ErrorMessage error = new ErrorMessage("Cannot remove global tag because internal service received exception for global tag "+id);
-			error.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
-			error.setInternalMessage("Cannot remove global tag because of an internal server error : "+e.getMessage());
-			ex.setStatus(Response.Status.INTERNAL_SERVER_ERROR);
-			ex.setErrMessage(error);
-			throw ex;			
+			log.debug("Generate exception using an ConddbService exception..."+e.getMessage());
+			String msg = "Error removing a globaltag resource: internal server exception !";
+			throw buildException(msg+" \n "+e.getMessage(), msg, Response.Status.INTERNAL_SERVER_ERROR);	
 		}
-		return resp;
 	}
 
 }
